372fb442d56c4d0f4b614ae67aed1afa8c9927c8
guusfelix2015 <gusfelix2015@gmail.com>
2025-10-02 23:12:03 -0300

    fix lint

--- src/hooks/auction.hooks.ts
+++ src/hooks/auction.hooks.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { auctionService } from '../services/auction.service';
 import type {
